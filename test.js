887b73c8811e735bf31516ab3ff5ad605fc2dba6
Toghrul <huseynlitogrul102@gmail.com>
2026-06-06 17:22:13 -0400

    feat: add CI pipeline

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 console.log('Starting tests...');
-setTimeout((console.log('Waiting 3 seconds...'), 3000));
+setTimeout(() => {
+    console.log("3 seconds");
+}, 3000);
 console.log('Tests complete...');
